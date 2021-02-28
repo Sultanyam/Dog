fbfecd269d6f24129f416cbf93dffe0ce34bf4d3
HP <HP@DESKTOP-L89B3LR>
2021-02-28 09:06:40 +0300

    Dog

---
+++ Dog/src/Dog.java
@@ -0,0 +1,35 @@
+
+public class Dog {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+breed beagle;
+size large;
+colour orange;
+age six;
+
+Dag(){
+	
+}
+Dog(breed newbeagle, size newlarge, colour neworange, age newsix){
+	beagle=newbeagle;
+	large=newlarge;
+	orange=neworange;
+	six=newsix;
+			
+	
+}
+breed Beagle() {
+	return beagle;
+}
+size Large() {
+return large;	
+}
+colour Orange() {
+	return orange;
+}
+	}
+	
+	
+
+}
